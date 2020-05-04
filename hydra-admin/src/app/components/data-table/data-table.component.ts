6f3221b76fdb35193c27a61bb7d1e66a50adf26e
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-05-05 00:35:36 +0100

    creating data table genenric component
    this component accept you to pass a array of objects and it renders columns and data dynamically.

--- hydra-admin/src/app/components/data-table/data-table.component.ts
+++ hydra-admin/src/app/components/data-table/data-table.component.ts
@@ -1,24 +1,8 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild, Input} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
-}
-
-/** Constants used to fill up our data base. */
-const COLORS: string[] = [
-  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
-  'aqua', 'blue', 'navy', 'black', 'gray'
-];
-const NAMES: string[] = [
-  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-];
+import { DataTable, DataTableColumns } from '../data-table-datasource';
 
 @Component({
   selector: 'app-data-table',
@@ -26,21 +10,27 @@ const NAMES: string[] = [
   styleUrls: ['./data-table.component.scss']
 })
 export class DataTableComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns: string[];
+  columns: DataTableColumns[];
+  
+  //input variable -> receives the data from a component to render the grid.
+  @Input() dataTable: DataTable;
 
+  //Generic dataSource --> Accept any type of array
+  dataSource: MatTableDataSource<any>;
+  
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  constructor() {
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
-  }
+  constructor() { }
 
   ngOnInit() {
+    //Bind the arrayObject to the grid
+    
+    this.columns = this.dataTable.columns;
+    this.displayedColumns = this.columns.map(c => c.columnDef);
+
+    this.dataSource = new MatTableDataSource(this.dataTable.dataSource);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
@@ -53,18 +43,4 @@ export class DataTableComponent implements OnInit {
       this.dataSource.paginator.firstPage();
     }
   }
-
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
 }
--- hydra-admin/src/app/modules/catalog/catalog-list/catalog-list.component.ts
+++ hydra-admin/src/app/modules/catalog/catalog-list/catalog-list.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { DataTable, DataTableColumns } from 'src/app/components/data-table-datasource';
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
+}
+
+// export interface DataTable {
+//   dataSource: UserData[],
+//   columns: ['id', 'name', 'progress', 'color'],
+//   display: ['Id', 'Name', 'My Progress', 'My Color']
+// }
+
+/** Constants used to fill up our data base. */
+const COLORS: string[] = [
+  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
+  'aqua', 'blue', 'navy', 'black', 'gray'
+];
+const NAMES: string[] = [
+  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
+  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+];
 
 @Component({
   selector: 'app-catalog-list',
@@ -6,10 +30,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalog-list.component.scss'],
 })
 export class CatalogListComponent implements OnInit {
+  users: UserData[];
+  dataTable: DataTable = new DataTable();
+  columns: DataTableColumns[]; //= ['id', 'name', 'progress', 'color'];
 
-  constructor() { }
+  constructor() {
+      // Create 100 users
+    this.users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+    this.createColumns();
+    this.dataTable.dataSource = this.users;
 
+    // Assign the data to the data source for the table to render
+  //  this.dataSource = new MatTableDataSource(users);
+  }
   ngOnInit(): void {
   }
 
+  createColumns(): void{
+    this.dataTable.columns = [
+      { columnDef: 'id', header: 'No.' },
+      { columnDef: 'name', header: 'Name'    },
+      { columnDef: 'progress', header: 'My Progress'  },
+      { columnDef: 'color', header: 'My Color'  },
+    ];
+  }
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+  };
 }
