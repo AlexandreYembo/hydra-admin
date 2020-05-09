b8ef62bf4584c41bc38ca92b07f988ac083f962c
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-05-09 10:32:08 +0100

    Update app-routing.module.ts

--- hydra-admin/src/app/app-routing.module.ts
+++ hydra-admin/src/app/app-routing.module.ts
@@ -3,19 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { CatalogListComponent } from './modules/catalog/catalog-list/catalog-list.component';
+import { CatalogEditComponent } from './modules/catalog/catalog-edit/catalog-edit.component';
 
-
-const routes: Routes = [{
-  path: '',
-  component: DefaultComponent,
-  children:[{
-    path: '',
-    component: DashboardComponent
-  },{
-    path:'catalog',
-    component: CatalogListComponent
-  }]
-}];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'catalog', component: CatalogListComponent,
+      children:[
+        { path:'new', component: CatalogListComponent }
+      ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
