11c58a4a0160c8b572615b71d9f94901edf58baf
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-11-25 22:48:16 +0000

    Adding Auth guard and protecting the routes

--- hydra-admin/src/app/app-routing.module.ts
+++ hydra-admin/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { CatalogListComponent } from './modules/catalog/catalog-list/catalog-list.component';
 import { CatalogEditComponent } from './modules/catalog/catalog-edit/catalog-edit.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', component: DashboardComponent, canActivate:[ AuthGuard ] },
   { path: 'catalog',
       children:[
-        { path:'', component: CatalogListComponent },
-        { path:'new', component: CatalogEditComponent }
+        { path:'', component: CatalogListComponent, canActivate:[ AuthGuard ] },
+        { path:'new', component: CatalogEditComponent, canActivate:[ AuthGuard ]  }
       ]
   },
   {path: 'login', component: AuthComponent}
