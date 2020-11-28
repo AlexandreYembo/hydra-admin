c117212ee863151cd13bdf9e0a209af6513d5455
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-11-28 09:39:33 +0000

    cleaning up

--- hydra-admin/src/app/components/data-table/store/models/data-source-state.ts
+++
@@ -1,5 +0,0 @@
-//import { DataTable } from '../../data-table-datasource';
-
-export interface DataSourceState<T> {
-    readonly dataSource: T[];//DataTable
-}
--- hydra-admin/src/app/modules/catalog/catalog-list/catalog-list.module.ts
+++ hydra-admin/src/app/modules/catalog/catalog-list/catalog-list.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataTableModule } from 'src/app/components/data-table/data-table.module';
 import { CatalogListComponent } from './catalog-list.component';
-import { StoreModule, Store } from '@ngrx/store';
-import { CatalogListFacade } from '../catalog-store';
-import { SharedStore } from 'src/app/store/shared-store';
 import { CatalogService } from '../catalog.service';
 
 @NgModule({
   declarations: [CatalogListComponent],
   imports: [
     CommonModule,
-    DataTableModule,
-    StoreModule.forRoot({
-      catalogy: CatalogListFacade
-    })
+    DataTableModule
   ],
-  providers:[Store, SharedStore, CatalogService],
+  providers:[CatalogService],
   exports:[
     CatalogListComponent
   ]
